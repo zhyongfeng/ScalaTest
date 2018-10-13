c23f1a77ea110663206e07bd20519153222c823d
zhyongfeng <zyf0808@yeah.net>
2018-10-13 19:30:40 +0800

    add some comment in the scala class

--- src/com/yfzhang/test/HelloWorld.scala
+++ src/com/yfzhang/test/HelloWorld.scala
@@ -1,5 +1,9 @@
 object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, Scala world!")
+  /* 这是我的第一个 Scala 程序
+   * Scala程序从main()方法开始处理，这是每一个Scala程序的强制程序入口部分。
+   * 以下程序将输出'Hello World!'
+   */
+  def main(args: Array[String]) {
+    println("Hello World!") // 输出 Hello World!
   }
 }
